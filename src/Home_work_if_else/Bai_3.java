270cdb21478e3839a10df338bdca2dbd39e0fd16
May <hoangquocanh2n6@gmail.com>
2025-12-25 22:51:56 +0700

    Check năm nhuận

---
+++ src/Home_work_if_else/Bai_3.java
@@ -0,0 +1,19 @@
+package Home_work_if_else;
+
+import java.util.Scanner;
+
+public class Bai_3{
+    public static void main(String[] a){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập vào năm : ");
+        int nam = sc.nextInt();
+
+        if(((nam % 4 == 0) && (nam % 100 != 0)) || (nam % 400 == 0)){
+            System.out.println(nam + " là năm nhuận.");
+        }
+        else{
+            System.out.println(nam + " ko phải năm nhuận.");
+        }
+    }
+}
