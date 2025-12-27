929bc91ef1ab0989ece0c422189aaf341489d6fc
May <hoangquocanh2n6@gmail.com>
2025-12-27 20:39:06 +0700

    Bài tập về toán tử 3 ngôi

---
+++ src/Home_work_if_else/Bai_6.java
@@ -0,0 +1,15 @@
+package Home_work_if_else;
+
+import java.util.Scanner;
+
+public class Bai_6{
+    public static void main(String[] a){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nhập vào điểm trung bình để kiểm tra học lực : ");
+
+        float diem = sc.nextFloat();
+
+        System.out.println((diem >= 8) ? "Giỏi" : (diem >= 6.5) ? "Khá" : (diem >= 5) ? "Trung bình" : "Yếu");
+    }
+}
