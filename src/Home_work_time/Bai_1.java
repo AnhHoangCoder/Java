3232d8cfdc6396850cf4069a165cf987af0be394
May <hoangquocanh2n6@gmail.com>
2025-12-31 14:02:57 +0700

    Bài tập về sử dụng time

---
+++ src/Home_work_time/Bai_1.java
@@ -0,0 +1,34 @@
+package Home_work_time;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Bai_1 {
+    public static void main(String[] argc){
+        Calendar cal = Calendar.getInstance();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập ngày tháng năm sinh của bạn : ");
+
+        String tmp = sc.nextLine();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        LocalDate birth = LocalDate.parse(tmp , dtf);
+
+        int day = birth.getDayOfMonth();
+        int month = birth.getMonthValue();
+        int year = birth.getYear();
+
+        System.out.println("Ngày tháng năm sinh của bạn là  : " + day + "/" + month + "/" + year);
+
+        //Xuất theo định dạng
+        SimpleDateFormat dinhdang = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss a");
+        //tạo 1 đối tượng date để get time trong đối tượng cal
+        Date d = cal.getTime();
+        String s = dinhdang.format(d);
+        System.out.println("Bây giờ là : " + s);
+    }
+}
