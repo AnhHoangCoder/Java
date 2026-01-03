b705b5e685891cdb0628783ea021b7e041d1b0b7
May <hoangquocanh2n6@gmail.com>
2026-01-03 10:03:51 +0700

    Kiểm tra user có nhập đúng username và password không

---
+++ src/Home_work_Arrays/Bai_5.java
@@ -0,0 +1,33 @@
+package Home_work_Arrays;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Bai_5 {
+    public static void main(String[] args) {
+        HashMap<String , String> dic = new HashMap<>();
+
+        for(int i = 1 ; i <= 10 ; i++){
+            dic.put("user" + i , "123456");
+        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhập username đăng nhập: ");
+        String username = input.nextLine();
+
+        System.out.print("Nhập password: ");
+        String password = input.nextLine();
+
+        if(dic.containsKey(username)){
+            if(dic.get(username).equals(password)){
+                System.out.println("Đăng nhập thành công");
+            }
+            else{
+                System.out.println("Sai password rồi ku");
+            }
+        }
+        else{
+            System.out.println("User không tồn tại");
+        }
+    }
+}
