7551ced814bb7f93f045c58f0283a341fda83587
May <hoangquocanh2n6@gmail.com>
2026-01-06 12:43:42 +0700

    Tính tổng giai thừa

---
+++ src/Home_work/java0104.java
@@ -0,0 +1,21 @@
+package Home_work;
+
+import java.util.Scanner;
+
+public class java0104 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        long[] arr = new long[n+1];
+        arr[1] = 1;
+        for(int i=2;i<=n;i++){
+            arr[i] = arr[i-1] * i;
+        }
+        long sum = 0;
+        for(int i=1;i<=n;i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
