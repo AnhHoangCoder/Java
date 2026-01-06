8d13c5a578a4a133be097524849f5df5ffc8d76d
May <hoangquocanh2n6@gmail.com>
2026-01-06 12:58:16 +0700

    Số thuận nghịch

---
+++ src/Home_work/java0106.java
@@ -0,0 +1,31 @@
+package Home_work;
+
+import java.util.Scanner;
+
+public class java0106 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for(int i = 0 ; i < t ; i++){
+            long n = sc.nextLong();
+            if(checkSoThuanNghich(n)){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+    }
+    public static boolean checkSoThuanNghich(long n){
+        String s = String.valueOf(n);
+        for(int i=0;i<s.length()/2;i++){
+            char c1 = s.charAt(i);
+            char c2 = s.charAt(s.length()-i-1);
+            if(c1 != c2){
+                return false;
+            }
+        }
+        return true;
+    }
+}
