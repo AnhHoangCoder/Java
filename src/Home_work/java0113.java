ecba8faf45f43789feff6142845f1b0badbb6120
May <hoangquocanh2n6@gmail.com>
2026-01-08 16:14:15 +0700

    Số may mắn

--- src/Home_work/java0113.java
+++ src/Home_work/java0113.java
@@ -1,4 +1,23 @@
 package Home_work;
 
+import java.util.Scanner;
+
 public class java0113 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            if(checkLucky(n)){
+                System.out.println(1);
+            }
+            else{
+                System.out.println(0);
+            }
+        }
+    }
+    public static boolean checkLucky(int n){
+        return String.valueOf(n).contains("86");
+    }
 }
