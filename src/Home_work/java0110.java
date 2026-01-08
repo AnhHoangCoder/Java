0cc24a6dc752d6471b9416e80b0b6abff9d6a5b0
May <hoangquocanh2n6@gmail.com>
2026-01-08 10:35:28 +0700

    Mã số quốc gia

---
+++ src/Home_work/java0110.java
@@ -0,0 +1,27 @@
+package Home_work;
+
+import java.util.Scanner;
+
+public class java0110 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for(int i = 0 ; i < t ; i++){
+            long n = sc.nextLong();
+            System.out.println(tfm(n));
+        }
+    }
+    public static String tfm(long n){
+        String s = String.valueOf(n);
+        StringBuilder sb = new StringBuilder(s);
+        String tmp = "084";
+
+        int idx;
+        while((idx = sb.indexOf(tmp)) != -1){
+            sb.delete(idx , idx + tmp.length());
+        }
+        return sb.toString();
+    }
+}
