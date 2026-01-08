0686f9ffd16ad161207b525e385905fe6e916469
May <hoangquocanh2n6@gmail.com>
2026-01-08 17:34:48 +0700

    Ước số chung nhỏ nhất - bộ số chung lớn nhất

---
+++ src/Home_work/java0121.java
@@ -0,0 +1,22 @@
+package Home_work;
+
+import java.util.Scanner;
+
+public class java0121 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while(t-- >0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            long USCLN = GCD(a,b);
+            long BSCNN = (a / USCLN) * b;
+            System.out.println(BSCNN + " " + USCLN);
+        }
+    }
+    public static long GCD(int a , int b){
+        if(b == 0) return (long)a;
+        return GCD(b , a%b);
+    }
+}
