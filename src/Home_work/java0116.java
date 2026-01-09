9888b998db52362fac9bbff9f60faaebd592b041
May <hoangquocanh2n6@gmail.com>
2026-01-09 09:58:20 +0700

    Ước số nguyên tố nhỏ nhất

--- src/Home_work/java0116.java
+++ src/Home_work/java0116.java
@@ -1,4 +1,36 @@
 package Home_work;
 
+import java.util.Scanner;
+
+
 public class java0116 {
+    public static void main(String[] args){
+        final int Max = 1_000_000;
+
+        int[] uPrime = new int[Max+1];
+        for(int i = 1 ; i <= Max ; i++){
+            uPrime[i] = i;
+        }
+
+        for(int i = 2 ; i*i <= Max ; i++){
+            if(uPrime[i] == i){
+                for(int j = i*i ; j <= Max ; j+=i){
+                    if(uPrime[j] == j){
+                        uPrime[j] = i;
+                    }
+                }
+            }
+        }
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while(t-- >0){
+            int n = sc.nextInt();
+            for(int i = 1 ; i <= n ; i++){
+                System.out.print(uPrime[i] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
