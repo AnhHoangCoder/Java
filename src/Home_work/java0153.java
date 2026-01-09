25654a7ad4d69fbb3c2a4ab5094fd4e42ce7fd46
May <hoangquocanh2n6@gmail.com>
2026-01-09 09:37:13 +0700

    Chia dư từ 1 -> n

---
+++ src/Home_work/java0153.java
@@ -0,0 +1,21 @@
+package Home_work;
+
+import java.util.Scanner;
+
+public class java0153 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while(t-- >0){
+            int n = sc.nextInt();
+            long k = sc.nextLong();
+
+            long sum = 0;
+            for(int i = 1 ; i <= n ; i++){
+                sum += (i%k);
+            }
+            System.out.println(sum);
+        }
+    }
+}
