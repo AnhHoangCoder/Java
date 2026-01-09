3fcb3ee8654e0d66c273e7e93f67cb2c12e552d7
May <hoangquocanh2n6@gmail.com>
2026-01-09 09:10:00 +0700

    Ước số nguyên tố thứ k

---
+++ src/Home_work/java0134.java
@@ -0,0 +1,34 @@
+package Home_work;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class java0134 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-- >0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int i = 2 ; i*i <= n ; i++){
+                while(n % i == 0){
+                    list.add(i);
+                    n /= i;
+                }
+            }
+            if(n>1){
+                list.add(n);
+            }
+            if(k > list.size()){
+                System.out.println(-1);
+            }
+            else{
+                System.out.println(list.get(k-1));
+            }
+        }
+    }
+}
