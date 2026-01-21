a8f82f8dce4727904e73582fc1ec37c29d9b5096
May <hoangquocanh2n6@gmail.com>
2026-01-21 22:36:58 +0700

    Ghi ra các số chia hết cho 7 nhưng ko phải là bội số của 5 trong khoảng từ a -> b

---
+++ src/Practice/bai_1.java
@@ -0,0 +1,32 @@
+package Practice;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class bai_1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập vào số nguyên a = ");
+        int a = sc.nextInt();
+        System.out.print("Nhập vào số nguyên b = ");
+        int b = sc.nextInt();
+
+        List<Integer> list = new ArrayList<Integer> ();
+        for(int i=a;i<=b;i++){
+            if((i % 7 == 0) && (i % 5 != 0)){
+                list.add(i);
+            }
+        }
+        showList(list);
+    }
+    public static void showList(List<Integer> list){
+        if(list != null && !list.isEmpty()){
+            int size = list.size();
+            for(int i=0;i<size-1;i++){
+                System.out.print(list.get(i) + ", ");
+            }
+            System.out.println(list.get(size-1));
+        }
+    }
+}
